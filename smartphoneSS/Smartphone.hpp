cc18fa3e517890dc9ec470e2d12869f8dd088861
Emilija Krstonošić <ekrstonosic@gmail.com>
2020-03-28 23:54:53 +0100

    dodatne metode

--- smartphoneSS/Smartphone.hpp
+++ smartphoneSS/Smartphone.hpp
@@ -1,6 +1,9 @@
 #ifndef SMARTPHONE_H_INCLUDED
 #define SMARTPHONE_H_INCLUDED
 
+#define PROGRESS_STEP 10
+#define PROGRESS_MAX 100
+
 #include "OpSystem.h"
 
 enum PhoneStates {sON, sOFF, sSTANDBY, sUPDATE};
@@ -19,6 +22,67 @@ public:
             state = state1;
             system = os;
             downloadingProgress = progress;
+    }
+    
+    PhoneStates getPhoneState() const {
+        return state;
+    }
+    OpSystem getOpSystem() const {
+        return system;
+    }
+
+    bool turnOff(){
+        if (state == sON){
+            state = sOFF;
+            return true;
+        }
+        return false;
+    }
+    bool turnOn(){
+        if (state == sOFF){
+            state = sON;
+            return true;
+        }
+        return false;
+    }
+    bool sleep(){
+        if (state == sON){
+            state = sSTANDBY;
+            return true;
+        }
+        return false;
+    }
+    bool wakeUp(){
+        if (state == sSTANDBY){
+            state = sON;
+            return true;
+        }
+        return false;
+    }
+
+    bool startUpdate(int newVersionNumber){
+        if (state != sOFF && state != sSTANDBY && state != sUPDATE){
+            state = sUPDATE;
+            downloadingProgress = 0;
+            system.setVersion(newVersionNumber);
+            return true;
+        }
+        return false;
+    }
+    bool downloadBatch(){
+        if (state == sUPDATE && downloadingProgress + PROGRESS_STEP <= PROGRESS_MAX){
+            downloadingProgress += PROGRESS_STEP;
+            return true;
+        }
+        return false;
+    }
+    bool finishUpdate(){
+        if (state == sUPDATE && downloadingProgress == 100){
+            state = sON;
+            downloadingProgress = -1;
+            return true;
+        }
+        return false;
     }
     
 };
